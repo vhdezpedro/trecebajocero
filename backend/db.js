400575ab06e0be9a4691c4fd640233da0fa03822
Pedro Villanueva <vhdezpedro@gmail.com>
2026-02-11 22:35:38 -0600

    Ajustando base de datos para deploy

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,11 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("Error conectadon la base de datos", err);
-    return;
-  }
-  console.log("Conectando con MySQL");
 });
 
 module.exports = db.promise();
