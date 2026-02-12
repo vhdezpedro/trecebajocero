91173468799ce79e766abfb975ae195061626499
Pedro Villanueva <vhdezpedro@gmail.com>
2026-02-11 23:34:54 -0600

    Se mueven los archivos y  carpetas a frontend

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{html, js}"],
-  safelist: ["text-green-500", "text-yellow-200"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
