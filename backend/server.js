6ebd426cd3ab155b65ae2a42ccf89c117d242be1
Pedro Villanueva <vhdezpedro@gmail.com>
2026-02-11 23:04:12 -0600

    se generar una get enn server.js para crear las tablas en la basede datos de la nube

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,36 @@ app.get("/", (req, res) => {
   return res.send("Servidor conectado");
 });
 
+app.get("/crear-tablas", async (req, res) => {
+  try {
+    db.query(
+      `CREATE TABLE IF NOT EXISTS clientes (
+      id int auto_increment primary key,
+      nombre varchar(100) not null,
+      telefono varchar(15),
+      fecha_nacimiento date,
+      creado_en timestamp default current_timestamp
+      )`,
+    );
+
+    db.query(
+      `CREATE TABLE IF NOT EXISTS visitas (
+      id int auto_increment primary key,
+      cliente_id int not null,
+      fecha_visita date not null,
+      creado_en timestamp default current_timestamp,
+      FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+      )`,
+    );
+
+    console.log("Las tablas se crearon correctamente");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+});
+
 app.get("/test-tablas", async (req, res) => {
   const [tablas] = await db.query("SHOW TABLES");
   res.json(tablas);
